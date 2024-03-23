837b0fcf34e0db2ce7827edfc7652f5c46cf4936
blcklamb <whenucan35@gmail.com>
2024-03-23 15:03:41 +0900

    💄 Button Component - add type(primary, secondary), edit state(default, disabled) - secondary css - onClick typing

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,36 +2,58 @@ import { TextStyle } from "@/types/text-style";
 
 const CSS_CONFIG = {
   green: {
-    default: "bg-green-500 text-white hover:bg-green-300",
-    disabled: "bg-grey-300 text-grey-500",
+    primary: {
+      default: "bg-green-500 text-white hover:bg-green-300",
+      disabled: "bg-grey-300 text-grey-500",
+    },
+    secondary: {
+      default:
+        "bg-green-50 border border-green-500 text-green-500 hover:bg-white",
+      disabled: "bg-white border border-grey-300 text-grey-500",
+    },
   },
   grey: {
-    default: "bg-grey-200 text-grey-900 hover:bg-grey-300",
-    disabled: "bg-grey-100 text-grey-400",
+    primary: {
+      default: "bg-grey-200 text-grey-900 hover:bg-grey-300",
+      disabled: "bg-grey-100 text-grey-400",
+    },
+    secondary: {
+      default: "bg-grey-200 text-grey-900 hover:bg-grey-300",
+      disabled: "bg-grey-100 text-grey-400",
+    },
   },
   yellow: {
-    default: "bg-yellow-500 text-white hover:bg-yellow-300",
-    disabled: "bg-grey-300 text-grey-500",
+    primary: {
+      default: "bg-yellow-500 text-white hover:bg-yellow-300",
+      disabled: "bg-grey-300 text-grey-500",
+    },
+    secondary: {
+      default:
+        "bg-yellow-50 border border-yellow-500 text-yellow-500 hover:bg-yellow-50",
+      disabled: "bg-white border border-grey-300 text-grey-500",
+    },
   },
 };
 
 export default function Button({
   color,
-  type,
+  type = "primary",
+  state,
   onClick,
   text,
   textStyle = "body2",
 }: {
   color: "green" | "grey" | "yellow";
-  type: "default" | "disabled";
-  onClick: () => void;
+  type?: "primary" | "secondary";
+  state: "default" | "disabled";
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
   text: string;
   textStyle?: TextStyle;
 }) {
   return (
     <button
-      disabled={type === "disabled"}
-      className={`rounded-lg px-4 h-full ${CSS_CONFIG[color][type]} ${textStyle}`}
+      disabled={state === "disabled"}
+      className={`rounded-lg w-full h-full ${CSS_CONFIG[color][type][state]} ${textStyle}`}
       onClick={onClick}
     >
       {text}
