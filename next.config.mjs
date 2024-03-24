887baf00ccdf3e3ff9923ca303656dcd10a648b0
blcklamb <whenucan35@gmail.com>
2024-03-24 14:43:41 +0900

    📦 MSW - install MSW - npx msw init ./public --save -> mockServiceWorker.js - add instrumentation.ts, webpack config for MSW server - add MSWConfig wrapper - add brower.ts server.ts depends on executed env

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,23 @@ const nextConfig = {
       },
     ],
   },
+  experimental: { instrumentationHook: true },
+  webpack(config, { isServer }) {
+    if (isServer) {
+      if (Array.isArray(config.resolve.alias)) {
+        config.resolve.alias.push({ name: "msw/browser", alias: false });
+      } else {
+        config.resolve.alias["msw/browser"] = false;
+      }
+    } else {
+      if (Array.isArray(config.resolve.alias)) {
+        config.resolve.alias.push({ name: "msw/node", alias: false });
+      } else {
+        config.resolve.alias["msw/node"] = false;
+      }
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
