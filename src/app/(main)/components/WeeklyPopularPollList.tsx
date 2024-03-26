7f66da8143159ca47489c6efb010a7f3bdce1e7d
blcklamb <whenucan35@gmail.com>
2024-03-26 18:09:48 +0900

    🔨 /poll/popular - add MSW handler - connect response with UI

--- src/app/(main)/components/WeeklyPopularPollList.tsx
+++ src/app/(main)/components/WeeklyPopularPollList.tsx
@@ -1,36 +1,101 @@
 import Image from "next/image";
 import Title from "./Title";
 import ColoredCircle from "@/components/common/ColoredCircle";
+import { fetchAPI } from "@/app/api/route";
+import Link from "next/link";
 
-export default function WeeklyPopularPollList() {
+interface GETPollListResponse {
+  poll_list: {
+    cover: string;
+    title: string;
+    author_list: string[];
+    publisher: string;
+    translator_list: string[];
+    vote_percentage: number;
+    vote_count: number;
+    opinion_count: number;
+  }[];
+}
+
+async function GETPollList() {
+  try {
+    const data = await fetchAPI<GETPollListResponse>(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/api/poll/popular`
+    );
+    return data.poll_list.map((ele, idx) => {
+      return {
+        pollId: idx,
+        cover: ele.cover,
+        title: ele.title,
+        authorList: ele.author_list,
+        publisher: ele.publisher,
+        translatorList: ele.translator_list,
+        votePercentage: ele.vote_percentage,
+        voteCount: ele.vote_count,
+        optionCount: ele.opinion_count,
+      };
+    });
+  } catch (error) {
+    console.error("Fail to fetch data: ", error);
+  }
+}
+
+export default async function WeeklyPopularPollList() {
+  const fetchedPollList = await GETPollList();
+
   return (
     <>
       <Title text="이번 주의 인기 투표" />
-      <div className="flex flex-col w-32 text-start">
-        <Image
-          width={128}
-          height={184}
-          className="rounded-lg"
-          alt="book-cover"
-          src={"https://image.yes24.com/goods/124418561/XL"}
-        />
-        <div className="flex">
-          <ColoredCircle width={20} percentage={100} />
-          <span className="body2-emphasis">85%</span>
-        </div>
-        <div className="body1 overflow-hidden whitespace-nowrap overflow-ellipsis h-6 pt-1">
-          조코딩의 챗GPT API를 활용한 수익형 웹 서비스 만들기
-        </div>
-        <div className="body2 text-[#757575] mb-7">
-          <span>조동근</span>
-          <span>{" ∙ "}</span>
-          <span>한빛미디어</span>
-        </div>
-        <div className="caption text-[#757575]">
-          <span>투표 {123}</span>
-          <span>{" ∙ "}</span>
-          <span>의견 {3}</span>
-        </div>
+      <div className="overflow-auto whitespace-nowrap">
+        {fetchedPollList &&
+          fetchedPollList.map((ele) => {
+            const {
+              cover,
+              pollId,
+              authorList,
+              optionCount,
+              publisher,
+              title,
+              translatorList,
+              voteCount,
+              votePercentage,
+            } = ele;
+            return (
+              <div className="inline-block w-32 text-start mr-4" key={pollId}>
+                <Link href={`/poll/${pollId}`}>
+                  <Image
+                    priority
+                    width={128}
+                    height={184}
+                    className="rounded-lg"
+                    alt={`book-cover-${title}`}
+                    src={cover}
+                    style={{ width: "auto", height: "100%" }}
+                  />
+                  <div className="flex">
+                    <ColoredCircle width={20} percentage={100} />
+                    <span className="body2-emphasis">{votePercentage}%</span>
+                  </div>
+                  <div className="body1 overflow-hidden whitespace-nowrap overflow-ellipsis h-6 pt-1">
+                    {title}
+                  </div>
+                  <div className="body2 overflow-hidden whitespace-nowrap overflow-ellipsis text-[#757575] mb-7">
+                    {authorList &&
+                      authorList.map((author, idx) => {
+                        return <span key={`${author}-${idx}`}>{author}</span>;
+                      })}
+                    <span>{" ∙ "}</span>
+                    <span>{publisher}</span>
+                  </div>
+                  <div className="caption text-[#757575]">
+                    <span>투표 {voteCount}</span>
+                    <span>{" ∙ "}</span>
+                    <span>의견 {optionCount}</span>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
       </div>
     </>
   );
