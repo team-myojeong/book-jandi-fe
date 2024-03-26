88be57064827912c1b042a2f20979b735f2e9e88
blcklamb <whenucan35@gmail.com>
2024-03-26 18:26:58 +0900

    💄  NewestPollList component - with static mock data

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/layout/Header";
 import WeeklyPopularPollList from "./components/WeeklyPopularPollList";
+import NewestPollList from "./components/NewestPollList";
 
 export default function Main() {
   return (
     <>
       <Header />
       <WeeklyPopularPollList />
+      <NewestPollList />
     </>
   );
 }
