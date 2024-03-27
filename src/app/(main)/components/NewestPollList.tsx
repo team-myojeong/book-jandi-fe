5aca11f93a41f491b8692d54a85716f73c6809fb
blcklamb <whenucan35@gmail.com>
2024-03-28 02:10:43 +0900

    🔨 /poll/recent - connect response with UI

--- src/app/(main)/components/NewestPollList.tsx
+++ src/app/(main)/components/NewestPollList.tsx
@@ -1,39 +1,101 @@
 import Image from "next/image";
 import Title from "./Title";
 import ColoredCircle from "@/components/common/ColoredCircle";
+import { fetchAPI } from "@/app/api/route";
+import Link from "next/link";
+
+interface GETPollListResponse {
+  poll_list: {
+    poll_id: number;
+    cover: string;
+    title: string;
+    author_list: string[];
+    publisher: string;
+    translator_list: string[];
+    vote_percentage: number;
+  }[];
+}
+
+async function GETPollList() {
+  try {
+    const data = await fetchAPI<GETPollListResponse>(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/api/poll/recent`
+    );
+
+    return data.poll_list.map((ele) => {
+      return {
+        pollId: ele.poll_id,
+        cover: ele.cover,
+        title: ele.title,
+        authorList: ele.author_list,
+        publisher: ele.publisher,
+        translatorList: ele.translator_list,
+        votePercentage: ele.vote_percentage,
+      };
+    });
+  } catch (error) {
+    console.error("Fail to fetch data: ", error);
+  }
+}
 
 export default async function NewestPollList() {
+  const fetchedPollList = await GETPollList();
+
+  const summaryList = (list: string[]) => {
+    if (!list) return `정보 없음`;
+    if (list.length > 1) return `${list[0]} 외 ${list.length - 1}인`;
+    return list[0];
+  };
+
   return (
-    <>
+    <div className="bg-white">
       <Title text="새로 올라온 투표" />
-      <div className="flex h-[133px] w-full gap-4 mt-4">
-        <Image
-          priority
-          width={93}
-          height={133}
-          className="rounded-lg"
-          alt={`book-cover`}
-          src={
-            "https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F927035%3Ftimestamp%3D20231227170332"
-          }
-          style={{ width: "auto", height: "100%" }}
-        />
-        <div className="text-start flex flex-col justify-center ">
-          <span className="body1">Clean Code(클린 코드)</span>
-          <div className="text-[#757575] mb-3">
-            <span className="caption">저자 로버트 C. 마틴</span>
-            <span>{" ∙ "}</span>
-            <span className="caption">번역 박재호</span>
-            <span>{" ∙ "}</span>
-            <span className="caption">인사이트</span>
-          </div>
-          <div className="flex">
-            <ColoredCircle width={20} percentage={100} />
-            <span className="body2">92%</span>
-          </div>
-        </div>
-      </div>
-      <hr className="border border-[#D9D9D9] mt-2" />
-    </>
+      {fetchedPollList &&
+        fetchedPollList.map((ele) => {
+          const {
+            pollId,
+            cover,
+            authorList,
+            publisher,
+            title,
+            translatorList,
+            votePercentage,
+          } = ele;
+          return (
+            <Link href={`/poll/${pollId}`} key={pollId}>
+              <div className="flex h-[133px] w-full gap-4 mt-4">
+                <Image
+                  priority
+                  width={93}
+                  height={133}
+                  className="rounded-lg"
+                  alt={`book-cover-${title}`}
+                  src={cover}
+                  style={{ width: "auto", height: "100%" }}
+                />
+                <div className="text-start flex flex-col justify-center ">
+                  <span className="body1">{title}</span>
+                  <div className="text-[#757575] mb-3">
+                    <span className="caption">
+                      저자 {summaryList(authorList)}
+                    </span>
+                    <span>{" ∙ "}</span>
+                    <span className="caption">
+                      번역 {summaryList(translatorList)}
+                    </span>
+                    <span>{" ∙ "}</span>
+                    <span className="caption">{publisher}</span>
+                  </div>
+                  <div className="flex">
+                    <ColoredCircle width={20} percentage={100} />
+                    <span className="body2">{votePercentage}%</span>
+                  </div>
+                </div>
+              </div>
+              <hr className="border border-[#D9D9D9] mt-2" />
+            </Link>
+          );
+        })}
+    </div>
   );
 }
