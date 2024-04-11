90519ac389e02f4c46e2a43c075fa6e5fbdcb7c3
blcklamb <whenucan35@gmail.com>
2024-04-11 14:40:05 +0900

    💄 refactor UI components and fix directory paths

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -4,32 +4,37 @@ const CSS_CONFIG = {
   green: {
     primary: {
       default: "bg-green-500 text-white hover:bg-green-300",
+      active: "",
       disabled: "bg-grey-300 text-grey-500",
     },
     secondary: {
-      default:
-        "bg-green-50 border border-green-500 text-green-500 hover:bg-white",
+      default: "text-green-500 border border-green-500 hover:bg-green-50",
+      active: "bg-green-50 border border-green-500 text-green-500",
       disabled: "bg-white border border-grey-300 text-grey-500",
     },
   },
   grey: {
     primary: {
       default: "bg-grey-200 text-grey-900 hover:bg-grey-300",
+      active: "",
       disabled: "bg-grey-100 text-grey-400",
     },
     secondary: {
       default: "bg-grey-200 text-grey-900 hover:bg-grey-300",
+      active: "",
       disabled: "bg-grey-100 text-grey-400",
     },
   },
   yellow: {
     primary: {
       default: "bg-yellow-500 text-white hover:bg-yellow-300",
+      active: "",
       disabled: "bg-grey-300 text-grey-500",
     },
     secondary: {
       default:
         "bg-yellow-50 border border-yellow-500 text-yellow-500 hover:bg-yellow-50",
+      active: "",
       disabled: "bg-white border border-grey-300 text-grey-500",
     },
   },
@@ -46,7 +51,7 @@ export default function Button({
 }: {
   color: "green" | "grey" | "yellow";
   type?: "primary" | "secondary";
-  state: "default" | "disabled";
+  state: "default" | "disabled" | "active";
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   text: string;
   textStyle?: TextStyle;
