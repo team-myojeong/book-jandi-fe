52b804201aa07ed044ee64391b8125637716c565
blcklamb <whenucan35@gmail.com>
2024-04-15 19:01:40 +0900

    ♻️ refactor actions and add new API endpoints

---
+++ src/actions/user.action.ts
@@ -0,0 +1,27 @@
+"use server";
+import { fetchAPI } from "@/apis/route";
+
+export interface SignUpForm {
+  job_id?: number;
+  career_id?: number;
+}
+
+interface POSTSignUpResponse {
+  success: boolean;
+}
+
+export async function POSTSignUp(requestBody: SignUpForm) {
+  try {
+    const result = await fetchAPI<POSTSignUpResponse>(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/api/user/signup`,
+      "POST",
+      "json",
+      {
+        ...requestBody,
+      }
+    );
+    return result.success;
+  } catch (error) {
+    console.error("Fail to fetch data:", error);
+  }
+}
