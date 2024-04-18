44a1e8139f8fdd3e746bb08bbf200fd9f1c7b634
blcklamb <whenucan35@gmail.com>
2024-04-18 23:12:08 +0900

    💄 fix button props component + add cn util

--- src/components/common/BottomFixedBottom.tsx
+++ src/components/common/BottomFixedBottom.tsx
@@ -1,19 +1,25 @@
 import Button from "./Button";
 
 interface BottomFixedBottomProps {
-  state: "default" | "disabled" | "active";
   text: string;
+  disabled?: boolean;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export default function BottomFixedBottom({
-  state,
   text,
+  disabled,
   onClick,
 }: BottomFixedBottomProps) {
   return (
-    <div className="w-[calc(100vw-2rem)] fixed h-32 pt-4 pb-16 bottom-0">
-      <Button onClick={onClick} color="green" state={state} text={text} />
+    <div className="fixed bottom-0 h-32 w-[calc(100vw-2rem)] max-w-[calc(500px-2rem)] pb-16 pt-4">
+      <Button
+        text={text}
+        color="green"
+        state={disabled ? "disabled" : "default"}
+        isDisabled={disabled}
+        onClick={onClick}
+      />
     </div>
   );
 }
