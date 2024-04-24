2dd9d4efa3c6540e9ef77787c6cce616a4f32341
blcklamb <whenucan35@gmail.com>
2024-04-24 21:54:11 +0900

    🔧 add image remotePatterns url for mock data

--- next.config.mjs
+++ next.config.mjs
@@ -26,6 +26,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   experimental: { instrumentationHook: true },
