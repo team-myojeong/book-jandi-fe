7b0c68b7e86ab9dd43fae6148a13a914fb405b1e
blcklamb <whenucan35@gmail.com>
2024-05-03 23:16:57 +0900

    🚧 hide console of middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,9 @@ export function middleware(request: NextRequest) {
   // Assume a "Cookie:nextjs=fast" header to be present on the incoming request
   // Getting cookies from the request using the `RequestCookies` API
   let cookie = request.cookies.get("token");
-  console.log(cookie); // => { name: 'nextjs', value: 'fast', Path: '/' }
+  // console.log(cookie); // => { name: 'nextjs', value: 'fast', Path: '/' }
   const allCookies = request.cookies.getAll();
-  console.log(allCookies); // => [{ name: 'nextjs', value: 'fast' }]
+  // console.log(allCookies); // => [{ name: 'nextjs', value: 'fast' }]
 
   request.cookies.has("nextjs"); // => true
   request.cookies.delete("nextjs");
@@ -22,7 +22,7 @@ export function middleware(request: NextRequest) {
     path: "/",
   });
   cookie = response.cookies.get("vercel");
-  console.log(cookie); // => { name: 'vercel', value: 'fast', Path: '/' }
+  // console.log(cookie); // => { name: 'vercel', value: 'fast', Path: '/' }
   // The outgoing response will have a `Set-Cookie:vercel=fast;path=/` header.
 
   return response;
