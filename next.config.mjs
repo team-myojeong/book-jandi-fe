7822441c110d39e44b9546f9d97fe6344295cdcc
blcklamb <whenucan35@gmail.com>
2024-05-06 22:50:41 +0900

    🩹 remove unused code and comments, and update dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -32,25 +32,31 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "http",
+        hostname: "k.kakaocdn.net",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
-  experimental: { instrumentationHook: true },
-  webpack(config, { isServer }) {
-    if (isServer) {
-      if (Array.isArray(config.resolve.alias)) {
-        config.resolve.alias.push({ name: "msw/browser", alias: false });
-      } else {
-        config.resolve.alias["msw/browser"] = false;
-      }
-    } else {
-      if (Array.isArray(config.resolve.alias)) {
-        config.resolve.alias.push({ name: "msw/node", alias: false });
-      } else {
-        config.resolve.alias["msw/node"] = false;
-      }
-    }
-    return config;
-  },
+  // experimental: { instrumentationHook: true },
+  // webpack(config, { isServer }) {
+  //   if (isServer) {
+  //     if (Array.isArray(config.resolve.alias)) {
+  //       config.resolve.alias.push({ name: "msw/browser", alias: false });
+  //     } else {
+  //       config.resolve.alias["msw/browser"] = false;
+  //     }
+  //   } else {
+  //     if (Array.isArray(config.resolve.alias)) {
+  //       config.resolve.alias.push({ name: "msw/node", alias: false });
+  //     } else {
+  //       config.resolve.alias["msw/node"] = false;
+  //     }
+  //   }
+  //   return config;
+  // },
 };
 
 export default nextConfig;
