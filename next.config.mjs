3c6f857cb5f6e6aa82fc4f497672171798eb9a4b
blcklamb <whenucan35@gmail.com>
2024-06-14 22:24:44 +0900

    ⚰️  Remove unused code and dependencies, and update Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -40,23 +40,6 @@ const nextConfig = {
       },
     ],
   },
-  // experimental: { instrumentationHook: true },
-  // webpack(config, { isServer }) {
-  //   if (isServer) {
-  //     if (Array.isArray(config.resolve.alias)) {
-  //       config.resolve.alias.push({ name: "msw/browser", alias: false });
-  //     } else {
-  //       config.resolve.alias["msw/browser"] = false;
-  //     }
-  //   } else {
-  //     if (Array.isArray(config.resolve.alias)) {
-  //       config.resolve.alias.push({ name: "msw/node", alias: false });
-  //     } else {
-  //       config.resolve.alias["msw/node"] = false;
-  //     }
-  //   }
-  //   return config;
-  // },
 };
 
 export default nextConfig;
