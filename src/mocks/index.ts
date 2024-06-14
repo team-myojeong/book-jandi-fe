11232da9b5aa6207ad62fed6d8dea40121513138
blcklamb <whenucan35@gmail.com>
2024-06-14 22:24:44 +0900

    ⚰️  Remove unused code and dependencies, and update Next.js configuration

--- src/mocks/index.ts
+++
@@ -1,9 +0,0 @@
-export async function initMSW() {
-  if (typeof window === "undefined") {
-    const { server } = await import("./server");
-    server.listen();
-  } else {
-    const { worker } = await import("./browser");
-    await worker.start({ onUnhandledRequest: "bypass" });
-  }
-}
