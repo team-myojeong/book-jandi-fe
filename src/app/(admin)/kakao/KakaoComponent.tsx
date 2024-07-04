95fbb5fa80c2007e074c745277ebd21cd088827f
blcklamb <whenucan35@gmail.com>
2024-07-04 13:57:30 +0900

    WIP : kakao login config

--- src/app/(admin)/kakao/KakaoComponent.tsx
+++ src/app/(admin)/kakao/KakaoComponent.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { redirect, useSearchParams } from "next/navigation";
-import { login } from "./actions";
-import { fetchAPI } from "@/apis/route";
 import { useEffect } from "react";
 
 export default function KakaoComponent() {
@@ -12,17 +10,25 @@ export default function KakaoComponent() {
 
   const asyncLogin = async (code: string) => {
     const param = new URLSearchParams({ code }).toString();
-    return await fetchAPI(
+    console.log("param", code);
+    return await fetch(
       `${process.env.NEXT_PUBLIC_SERVER_URL}/user/login?${param}`,
-      "POST",
-      "json",
+      {
+        method: "GET",
+      },
     );
   };
   useEffect(() => {
     if (authCode) {
-      asyncLogin(authCode as string).then((response) => {
-        console.log("response", response);
-      });
+      asyncLogin(authCode as string)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          // TODO: 로그인 성공 시 access token 들고 다니기~
+          console.log("data", data);
+          return;
+        });
     } else if (kakaoServerError) {
       redirect("/notifications/authentication-failed");
     }
